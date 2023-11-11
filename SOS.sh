4a046f29019193a177ef25b79bd785e0e0f4feed
Xkaliberos <146337230+Xkaliberos@users.noreply.github.com>
2023-11-12 02:38:23 +0530

    Create SOS.sh

---
+++ SOS.sh
@@ -0,0 +1,24 @@
+   sudo apt update
+   sudo apt upgrade
+   sudo apt update -y
+   sudo apt upgrade -y
+   sudo apt dist-upgrade
+   sudo apt full-upgrade -7
+   sudo apt autoremove -y
+   sudo apt autoclean
+   sudo fwupdmgr get-devices -y
+   sudo fwupdmgr get-updates -y
+   sudo fwupdmgr update -y
+   flatpak update -y
+   sudo apt clean
+   sudo apt update -m
+   sudo dpkg --configure -a
+   sudo apt install -f
+   sudo apt full-upgrade
+   sudo apt autoremove --purge
+   sudo apt-get update -y
+   sudo apt-get upgrade -y
+   apt-get full-upgrade -y
+   sudo apt-get dist-upgrade -y
+   sudo apt-get autoremove -y
+   sudo apt-get autoclean -y
